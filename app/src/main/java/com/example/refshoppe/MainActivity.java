f5f9658cb7c970414c0052d21d389fe6fe75d447
Sabrina Li <sabrinali0312@gmail.com>
2020-02-24 12:27:03 -0500

    add bottom navigation bar

--- app/src/main/java/com/example/refshoppe/MainActivity.java
+++ app/src/main/java/com/example/refshoppe/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.navigation);
+        NavController navController = Navigation.findNavController(this, R.id.fragment_container);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
