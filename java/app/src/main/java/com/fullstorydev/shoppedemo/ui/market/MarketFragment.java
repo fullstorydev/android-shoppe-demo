d995f7c27e16d868bb6f4efe84ee891aec181be0
Sabrina Li <sabrinali0312@gmail.com>
2020-03-03 16:04:03 -0500

    update layout and remove boilerplate view

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
@@ -21,13 +21,13 @@ public class MarketFragment extends Fragment {
         marketViewModel =
                 ViewModelProviders.of(this).get(MarketViewModel.class);
         View root = inflater.inflate(R.layout.fragment_market, container, false);
-        final TextView textView = root.findViewById(R.id.text_market);
-        marketViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_market);
+//        marketViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
