0153157b18081be2865ac1cc035bd7f0b1c1bb25
Sabrina Li <sabrinali0312@gmail.com>
2020-03-04 12:26:46 -0500

    add network and json utils

---
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/utilities/NetworkUtils.java
@@ -0,0 +1,32 @@
+package com.fullstorydev.shoppedemo.utilities;
+
+import com.fullstorydev.shoppedemo.data.Product;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetworkUtils {
+    private static final OkHttpClient mClient = new OkHttpClient();
+
+    public static String getProductListFromURL(String urlStr) throws IOException {
+        Request request = new Request.Builder()
+                            .url(urlStr)
+                            .build();
+
+        try (Response response = mClient.newCall(request).execute()) {
+            if(response.isSuccessful()){
+                return response.body().string();
+            }else{
+                throw new IOException("Unexpected response code " + response.code());
+            }
+        }
+    }
+}
