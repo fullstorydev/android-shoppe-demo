9c9a9c423d6a4bf2298e602757cdc510ba20685d
Sabrina Li <sabrinali0312@gmail.com>
2020-03-09 16:56:29 -0400

    load data from API in product repository

--- java/app/src/main/java/com/fullstorydev/shoppedemo/data/ProductRepository.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/data/ProductRepository.java
@@ -1,15 +1,25 @@
 package com.fullstorydev.shoppedemo.data;
 
 import android.app.Application;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
+import com.fullstorydev.shoppedemo.R;
+import com.fullstorydev.shoppedemo.utilities.JsonHelper;
+import com.fullstorydev.shoppedemo.utilities.NetworkUtils;
+import com.google.gson.JsonParseException;
+
+import java.io.IOException;
 import java.util.List;
 
 public class ProductRepository {
 
     private ProductDao mProductDao;
-    private LiveData<List<Product>> mAllProducts;
+    private LiveData<List<Product>> mAllItems; //items are in cart
+    private MutableLiveData<List<Product>> mAllProducts; //products are sold in shop, fetched from API
 
     // Note that in order to unit test the class, you have to remove the Application
     // dependency. This adds complexity and much more code, and this sample is not about testing.
@@ -18,12 +28,38 @@ public class ProductRepository {
     public ProductRepository(Application application) {
         AppDatabase db = AppDatabase.getDatabase(application);
         mProductDao = db.productDao();
-        mAllProducts = mProductDao.getAll();
+        mAllItems = mProductDao.getAll();
+        String hostURLStr = Resources.getSystem().getString(R.string.aws_host);
+        new FetchFromAPI().execute(hostURLStr);
     }
 
-    public LiveData<List<Product>> getAll() {
-        return mAllProducts;
+    class FetchFromAPI extends AsyncTask<String,Void,List<Product>> {
+        @Override
+        protected List<Product> doInBackground(String... urls) {
+            if(urls.length>0){
+                try{
+                    NetworkUtils.getProductListFromURL(urls[0]);
+                    return JsonHelper.getProductListFromJsonString(urls[0]);
+                }catch (IOException | JsonParseException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute( List<Product> list) {
+            if(list != null){
+                mAllProducts.postValue(list);
+            }
+        }
     }
+
+    public LiveData<List<Product>> getAllFromDB() {
+        return mAllItems;
+    }
+
+    public LiveData<List<Product>> getAllFromAPI() { return mAllProducts; }
 
     public void insert(Product product) {
         AppDatabase.databaseWriteExecutor.execute(() -> {
--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
@@ -9,8 +9,7 @@ public class MarketViewModel extends ViewModel {
     private MutableLiveData<String> mText;
 
     public MarketViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is market fragment");
+
     }
 
     public LiveData<String> getText() {
