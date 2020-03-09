f2a09c327fc59b6aca6abe2e05cde25a5d9a8d5d
Sabrina Li <sabrinali0312@gmail.com>
2020-03-09 10:41:29 -0400

    move code to get view model in onactivitycreated and comment on placeholder code

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
@@ -1,6 +1,8 @@
 package com.fullstorydev.shoppedemo.ui.market;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import com.fullstorydev.shoppedemo.R;
 
@@ -17,11 +20,16 @@ public class MarketFragment extends Fragment {
     private MarketViewModel marketViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        marketViewModel =
-                ViewModelProviders.of(this).get(MarketViewModel.class);
+                             ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_market, container, false);
-        
+
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // get ViewModel from ViewModelProvider, placeholder for observing LiveData later
+        marketViewModel = new ViewModelProvider(this).get(MarketViewModel.class);
     }
 }
--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MarketViewModel extends ViewModel {
-
+    //placeholder code to be replaced once data logic in place
     private MutableLiveData<String> mText;
 
     public MarketViewModel() {
