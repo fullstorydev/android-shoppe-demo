8060b475ac99b786b96c3325d1fb983ed361420c
Sabrina Li <sabrinali0312@gmail.com>
2020-03-09 17:01:39 -0400

    rename market product binding adapter to more generic to be used for both market and cart

--- java/app/src/main/java/com/fullstorydev/shoppedemo/adapters/ProductBingdingAdapter.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/adapters/ProductBingdingAdapter.java
@@ -4,7 +4,7 @@ import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
 
-public class MarketProductBingdingAdapter {
+public class ProductBingdingAdapter {
     @BindingAdapter({"imageURL"})
     public static void setImageURL(ImageView view, String url) {
         //place holder adapter for loading the image from url and set it to the view
