296391d1f5b5783cd757eefe765f4bee022c6420
sabrina-li <sabrinali0312@gmail.com>
2020-03-09 11:03:56 -0400

    Sabrina/market layout (#12)
    * add layout res for market fragment
    
    * update layout and remove boilerplate view
    
    * remove comment
    
    * adding layout and some properties to market_layout
    
    * add remove from cart sting
    
    * move code to get view model in onactivitycreated and comment on placeholder code
    
    * remove unused packages

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import com.fullstorydev.shoppedemo.R;
 
 public class MarketFragment extends Fragment {
@@ -17,17 +16,16 @@ public class MarketFragment extends Fragment {
     private MarketViewModel marketViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        marketViewModel =
-                ViewModelProviders.of(this).get(MarketViewModel.class);
+                             ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_market, container, false);
-        final TextView textView = root.findViewById(R.id.text_market);
-        marketViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // get ViewModel from ViewModelProvider, placeholder for observing LiveData later
+        marketViewModel = new ViewModelProvider(this).get(MarketViewModel.class);
     }
 }
