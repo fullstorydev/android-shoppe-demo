dd594ee02e6a81383645f55f1f56c4c987fe20de
Sabrina Li <sabrinali0312@gmail.com>
2020-03-09 10:46:43 -0400

    remove unused packages

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
@@ -1,18 +1,14 @@
 package com.fullstorydev.shoppedemo.ui.market;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import com.fullstorydev.shoppedemo.R;
 
 public class MarketFragment extends Fragment {
