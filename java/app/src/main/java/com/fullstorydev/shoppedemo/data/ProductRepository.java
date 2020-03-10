0d8a0fb752b4824252fdd29e423a6bbf7d80ce9d
sabrina-li <sabrinali0312@gmail.com>
2020-03-10 11:40:35 -0400

    Sabrina/loadfrom api (#17)
    * load data from API in product repository
    
    * repository to load from API, pass through view model and market fragment to get the list of products from API
    
    * add comment on app architecture
    
    * add comment in repository
    
    * typo in comment
    
    * move from AsyncTask to new thread as no need to manipulate UI thread here

--- java/app/src/main/java/com/fullstorydev/shoppedemo/data/ProductRepository.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/data/ProductRepository.java
@@ -3,27 +3,57 @@ package com.fullstorydev.shoppedemo.data;
 import android.app.Application;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
+import com.fullstorydev.shoppedemo.R;
+import com.fullstorydev.shoppedemo.utilities.JsonHelper;
+import com.fullstorydev.shoppedemo.utilities.NetworkUtils;
+import com.google.gson.JsonParseException;
+
+import java.io.IOException;
 import java.util.List;
 
 public class ProductRepository {
 
     private ProductDao mProductDao;
-    private LiveData<List<Product>> mAllProducts;
+    private LiveData<List<Product>> mAllItems; //items are in cart
+    private MutableLiveData<List<Product>> mAllProducts; //products are sold in shop, fetched from API
 
     // Note that in order to unit test the class, you have to remove the Application
     // dependency. This adds complexity and much more code, and this sample is not about testing.
     // See the BasicSample in the android-architecture-components repository at
     // https://github.com/googlesamples
+
+    // The repository is the only class that depends on multiple other classes; in this example, the repository depends on a persistent data model and a remote backend data source.
+    // For more information on this app architecture, see: https://developer.android.com/jetpack/docs/guide
     public ProductRepository(Application application) {
         AppDatabase db = AppDatabase.getDatabase(application);
         mProductDao = db.productDao();
-        mAllProducts = mProductDao.getAll();
+        mAllItems = mProductDao.getAll();
+        mAllProducts = new MutableLiveData<>();
+        
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String hostURLStr = application.getString(R.string.products_endpoint);
+                try{
+                    String resStr = NetworkUtils.getProductListFromURL(hostURLStr);
+                    List<Product> list = JsonHelper.getProductListFromJsonString(resStr);
+                    mAllProducts.postValue(list);
+                }catch (IOException | JsonParseException e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
-    public LiveData<List<Product>> getAll() {
-        return mAllProducts;
+    // get items from cart - data saved in DB
+    public LiveData<List<Product>> getAllItemsFromDB() {
+        return mAllItems;
     }
+
+    // get products for market - data fetched from REST API via a async task
+    public LiveData<List<Product>> getAllFromAPI() { return mAllProducts; }
 
     public void insert(Product product) {
         AppDatabase.databaseWriteExecutor.execute(() -> {
--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
@@ -1,19 +1,28 @@
 package com.fullstorydev.shoppedemo.ui.market;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class MarketViewModel extends ViewModel {
-    //placeholder code to be replaced once data logic in place
-    private MutableLiveData<String> mText;
+import com.fullstorydev.shoppedemo.data.Product;
+import com.fullstorydev.shoppedemo.data.ProductRepository;
 
-    public MarketViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is market fragment");
+import java.util.List;
+
+public class MarketViewModel extends AndroidViewModel {
+    private ProductRepository mRepository;
+    private LiveData<List<Product>> mProductList;
+
+
+    public MarketViewModel(Application application) {
+        super(application);
+
+        mRepository = new ProductRepository(application);
+        mProductList = mRepository.getAllFromAPI();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<Product>> getProductList() {
+        return mProductList;
     }
 }
