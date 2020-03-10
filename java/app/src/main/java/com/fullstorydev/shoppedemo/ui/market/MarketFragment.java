03a96c5567028170b2464612304dd0e9a46d4ec3
Sabrina Li <sabrinali0312@gmail.com>
2020-03-10 14:07:46 -0400

    add recyclerViewAdapter and poputate recycler view

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
@@ -9,20 +9,28 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.fullstorydev.shoppedemo.R;
+import com.fullstorydev.shoppedemo.adapters.MarketProductAdapter;
 import com.fullstorydev.shoppedemo.data.Product;
+import com.fullstorydev.shoppedemo.databinding.ListItemMarketBinding;
 
 import java.util.List;
 
 public class MarketFragment extends Fragment {
 
     private MarketViewModel marketViewModel;
+    private MarketProductAdapter mMarketProductAdapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_market, container, false);
+
+        mMarketProductAdapter = new MarketProductAdapter();
+        RecyclerView mRecyclerView = root.findViewById(R.id.rv_product);
+        mRecyclerView.setAdapter(mMarketProductAdapter);
 
         return root;
     }
@@ -32,11 +40,10 @@ public class MarketFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         marketViewModel = new ViewModelProvider(this).get(MarketViewModel.class);
-
         marketViewModel.getProductList().observe(this.getViewLifecycleOwner(), new Observer<List<Product>>() {
             @Override
             public void onChanged(List<Product> products) {
-                // products holds the list of products, live data is updated when retrieved from API, this data will be used to populate the recycler view
+                mMarketProductAdapter.setProductList(products);
             }
         });
     }
