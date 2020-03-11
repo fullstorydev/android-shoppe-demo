308bc2e5fa0d5f9de83342f9007f83c64296178a
Sabrina Li <sabrinali0312@gmail.com>
2020-03-11 16:03:52 -0400

    add view model and observer for the cart fragment

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/cart/CartViewModel.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/cart/CartViewModel.java
@@ -1,19 +1,32 @@
 package com.fullstorydev.shoppedemo.ui.cart;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class CartViewModel extends ViewModel {
-    //placeholder code to be replaced once data logic in place
-    private MutableLiveData<String> mText;
+import com.fullstorydev.shoppedemo.data.Product;
+import com.fullstorydev.shoppedemo.data.ProductRepository;
 
-    public CartViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is cart fragment");
+import java.util.List;
+
+public class CartViewModel extends AndroidViewModel {
+    private ProductRepository mRepository;
+    private LiveData<List<Product>> mItemList;
+
+    public CartViewModel(Application application) {
+        super(application);
+        mRepository = new ProductRepository(application);
+        mItemList = mRepository.getAllItemsFromDB();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<Product>> getItemList() {
+        return mItemList;
+    }
+
+    public void decreaseQuantityInCart(Product product){
+        mRepository.decreaseQuantityInCart(product);
     }
 }
