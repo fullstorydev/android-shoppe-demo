0b4bfa7141072cf119c4fded58d817e62b29b48b
sabrina-li <sabrinali0312@gmail.com>
2020-03-11 10:07:51 -0400

    Sabrina/recycler view adapter (#18)
    * add recyclerViewAdapter and poputate recycler view
    
    * add binding for onclick listener

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketFragment.java
@@ -9,20 +9,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.fullstorydev.shoppedemo.R;
+import com.fullstorydev.shoppedemo.adapters.MarketProductAdapter;
 import com.fullstorydev.shoppedemo.data.Product;
 
 import java.util.List;
 
-public class MarketFragment extends Fragment {
+public class MarketFragment extends Fragment implements MarketEventHandlers {
 
     private MarketViewModel marketViewModel;
+    private MarketProductAdapter mMarketProductAdapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_market, container, false);
+
+        mMarketProductAdapter = new MarketProductAdapter(this);
+        RecyclerView mRecyclerView = root.findViewById(R.id.rv_product);
+        mRecyclerView.setAdapter(mMarketProductAdapter);
 
         return root;
     }
@@ -32,12 +39,15 @@ public class MarketFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         marketViewModel = new ViewModelProvider(this).get(MarketViewModel.class);
-
         marketViewModel.getProductList().observe(this.getViewLifecycleOwner(), new Observer<List<Product>>() {
             @Override
             public void onChanged(List<Product> products) {
-                // products holds the list of products, live data is updated when retrieved from API, this data will be used to populate the recycler view
+                mMarketProductAdapter.setProductList(products);
             }
         });
+    }
+
+    public void onClickAddToCart(Product product){
+        marketViewModel.increaseQuantityInCart(product);
     }
 }
--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/market/MarketViewModel.java
@@ -14,15 +14,17 @@ public class MarketViewModel extends AndroidViewModel {
     private ProductRepository mRepository;
     private LiveData<List<Product>> mProductList;
 
-
     public MarketViewModel(Application application) {
         super(application);
-
         mRepository = new ProductRepository(application);
         mProductList = mRepository.getAllFromAPI();
     }
 
     public LiveData<List<Product>> getProductList() {
         return mProductList;
+    }
+
+    public void increaseQuantityInCart(Product product){
+        mRepository.increaseQuantityInCart(product);
     }
 }
