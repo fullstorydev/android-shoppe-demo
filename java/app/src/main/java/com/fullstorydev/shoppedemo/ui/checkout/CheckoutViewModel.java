38bf5fc8503621125f5b2dae7cfbf407ed6b104a
Sabrina Li <sabrinali0312@gmail.com>
2020-03-24 14:24:23 -0400

    add vm java with setters for each textedit

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/checkout/CheckoutViewModel.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/checkout/CheckoutViewModel.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.fullstorydev.shoppedemo.data.CustomerInfo;
 import com.fullstorydev.shoppedemo.data.CustomerInfoRepository;
@@ -15,7 +14,7 @@ import com.fullstorydev.shoppedemo.utilities.Constants;
 public class CheckoutViewModel extends AndroidViewModel {
     private CustomerInfoRepository mCustomerInfoRepo;
     private LiveData<Boolean> isLoading;
-    private CustomerInfo info;
+    private CustomerInfo customerInfo;
 
 
     public CheckoutViewModel(Application application) {
@@ -23,57 +22,80 @@ public class CheckoutViewModel extends AndroidViewModel {
 
         mCustomerInfoRepo = new CustomerInfoRepository(application);
         isLoading = mCustomerInfoRepo.getIsLoading();
-        info = mCustomerInfoRepo.getCustomerInfo(); //nullable
+        customerInfo = new CustomerInfo.OrderBuilder().buildOrder(); // init to new customerInfo Obj with empty values
     }
 
     public LiveData<Boolean> getIsLoading() { return isLoading; }
-    public void fetchCustomerInfo() { info = mCustomerInfoRepo.getCustomerInfo(); }
-
-    public CustomerInfo getInfo(){ return info; }
-
+    public CustomerInfo getCustomerInfo(){ return customerInfo; }
     public String[] getStates() { return mCustomerInfoRepo.getStates(); }
     public Integer[] getYears() { return mCustomerInfoRepo.getYears(); }
     public Integer[] getMonths() { return mCustomerInfoRepo.getMonths(); }
 
-//    public String getFirstName(){ return _info.getFirstName(); }
-    public void setFirstName(CharSequence s, int arg1, int arg2, int arg3) {
-        Log.d("here","set first name");
-        String firstName = String.valueOf(s);
-        if(!firstName.equals(info.getFirstName())){
-            info.setFirstName(firstName);
-            mCustomerInfoRepo.updateCustomerInfo(Constants.FIRST_NAME,firstName);
+    public void fetchCustomerInfo() { customerInfo = mCustomerInfoRepo.getCustomerInfo(); }
+
+    public void setFirstName(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getFirstName())){
+            customerInfo.setFirstName(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.FIRST_NAME,str);
+        }
+    }
+    public void setLastName(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getLastName())){
+            customerInfo.setLastName(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.LAST_NAME,str);
+        }
+    }
+    public void setAddress1(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getAddress1())){
+            customerInfo.setAddress1(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.ADDRESS_1,str);
+        }
+    }
+    public void setAddress2(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getAddress2())){
+            customerInfo.setAddress2(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.ADDRESS_2,str);
+        }
+    }
+    public void setCity(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getCity())){
+            customerInfo.setCity(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.CITY,str);
         }
     }
 
-//
-//    public String getLastName() { return _info.getLastName(); }
-//    public void setLastName(String lastName) { _info.setLastName(lastName);}
-//
-//    public String getAddress1() { return _info.getAddress1(); }
-//    public void setAddress1(String address1) { _info.setAddress1(address1);}
-//
-//    public String getAddress2() { return _info.getAddress2(); }
-//    public void setAddress2(String address2) { _info.setAddress2(address2); info.setValue(_info);}
-//
-//    public String getCity() { return _info.getCity(); }
-//    public void setCity(String city) { _info.setCity(city); info.setValue(_info);}
-//
-//    public int getState() { return _info.getState(); }
 //    public void setState(int state) { _info.setState(state); info.setValue(_info);}
-//
-//    public String getZip() { return _info.getZip(); }
-//    public void setZip(String zip) { _info.setZip(zip); info.setValue(_info);}
-//
-//    public String getCreditCardNumber() { return _info.getCreditCardNumber(); }
-//    public void setCreditCardNumber(String creditCardNumber) { _info.setCreditCardNumber(creditCardNumber); info.setValue(_info);}
-//
-//    public int getExpirationMonth() { return _info.getExpirationMonth(); }
+
+    public void setZip(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getZip())){
+            customerInfo.setZip(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.ZIP,str);
+        }
+    }
+    public void setCreditCardNumber(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getCreditCardNumber())){
+            customerInfo.setCreditCardNumber(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.CREDIT_CARD_NUMBER,str);
+        }
+    }
+
 //    public void setExpirationMonth(int expirationMonth) { _info.setExpirationMonth(expirationMonth); info.setValue(_info);}
-//
-//    public int getExpirationYear() { return _info.getExpirationYear(); }
+
 //    public void setExpirationYear(int expirationYear) { _info.setExpirationYear(expirationYear); info.setValue(_info);}
-//
-//    public String getSecurityCode() { return _info.getSecurityCode(); }
-//    public void setSecurityCode(String securityCode) { _info.setSecurityCode(securityCode); info.setValue(_info);}
+
+    public void setSecurityCode(CharSequence s, int arg1, int arg2, int arg3) { // handler for EditText onTextChanged
+        String str = String.valueOf(s);
+        if(!str.equals(customerInfo.getSecurityCode())){
+            customerInfo.setSecurityCode(str);
+            mCustomerInfoRepo.updateCustomerInfo(Constants.SECURITY_CODE,str);
+        }
+    }
 
 }
