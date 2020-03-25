3532919690f3650d453929411eb333e6b5c4fb99
Sabrina Li <sabrinali0312@gmail.com>
2020-03-25 10:37:42 -0400

    add binding to checkout fragment

--- java/app/src/main/java/com/fullstorydev/shoppedemo/ui/checkout/CheckoutFragment.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/checkout/CheckoutFragment.java
@@ -7,20 +7,25 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.fullstorydev.shoppedemo.R;
+import com.fullstorydev.shoppedemo.databinding.FragmentCheckoutBinding;
+import com.fullstorydev.shoppedemo.databinding.ListItemMarketBinding;
 import com.fullstorydev.shoppedemo.ui.cart.CartViewModel;
 
 public class CheckoutFragment extends Fragment {
-
+    FragmentCheckoutBinding binding;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_checkout, container, false);
+//        View view = inflater.inflate(R.layout.fragment_checkout, container, false);
 
-        return view;
+         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_checkout, container, false);
+
+        return binding.getRoot();
     }
 
     @Override
@@ -28,5 +33,15 @@ public class CheckoutFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         CheckoutViewModel vm = new ViewModelProvider(this).get(CheckoutViewModel.class);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+
+        vm.getIsLoading().observe(getViewLifecycleOwner(),isLoading->{
+            if(!isLoading){
+                vm.fetchCustomerInfo();
+                binding.setViewmodel(vm);
+                binding.executePendingBindings();
+            }
+        });
+
     }
 }
