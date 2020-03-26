510cee076a86780374f59e242618cbafdeae85a0
Sabrina Li <sabrinali0312@gmail.com>
2020-03-26 14:49:28 -0400

    add FS

---
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/App.java
@@ -0,0 +1,10 @@
+package com.fullstorydev.shoppedemo;
+
+import androidx.multidex.MultiDexApplication;
+public class App extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //custom code
+    }
+}
