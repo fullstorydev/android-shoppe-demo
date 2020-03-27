e3738496380d27832453f0789d53d6ce8e727624
sabrina-li <sabrinali0312@gmail.com>
2020-03-27 09:51:28 -0400

    add fragment java (#32)

---
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/checkout/CheckoutFragment.java
@@ -0,0 +1,22 @@
+package com.fullstorydev.shoppedemo.ui.checkout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.fullstorydev.shoppedemo.R;
+
+public class CheckoutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_checkout, container, false);
+
+        return view;
+    }
+}
