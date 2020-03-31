f94fb1b374ee2d0e18c15d60fbd8c292e51c8da2
sabrina-li <sabrinali0312@gmail.com>
2020-03-31 15:11:23 -0400

    add onclick listener for purchase (#41)
    * add onclick listener for purchase
    
    * white spaces and catching specific errors

---
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/ui/checkout/CheckoutEventHandlers.java
@@ -0,0 +1,9 @@
+package com.fullstorydev.shoppedemo.ui.checkout;
+
+import android.view.View;
+
+import com.fullstorydev.shoppedemo.data.CustomerInfo;
+
+public interface CheckoutEventHandlers {
+    void onClickPurchase(CustomerInfo customerInfo, Double subtotal);
+}
