a770ba3862f14d483dfae769b76df15569bb3d77
sabrina-li <sabrinali0312@gmail.com>
2020-04-01 15:43:34 -0400

    hide keyboard (#43)
    * hide keyboard
    
    * remove activity package: unused

--- java/app/src/main/java/com/fullstorydev/shoppedemo/MainActivity.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.fullstorydev.shoppedemo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -46,5 +50,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         return NavigationUI.navigateUp(mNavController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    // hide the keyboard when user click on anywhere else
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if(view != null && view.requestFocus()){
+            hideKeyboard(view);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if(imm != null){
+            imm.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
