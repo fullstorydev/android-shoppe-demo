cbed5df199b509790b487495662739e360778417
sabrina-li <sabrinali0312@gmail.com>
2020-04-07 13:15:16 -0400

    change column name to cart_item_id

--- kotlin/app/src/main/java/com/fullstorydev/shoppedemo/data/CartItem.kt
+++ kotlin/app/src/main/java/com/fullstorydev/shoppedemo/data/CartItem.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "cart_items")
 data class CartItem (
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "item_id")  val id: Int,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "cart_item_id")  val id: Int,
     @Embedded val product: Product,
     val quantityInCart: Int = 0
 )
