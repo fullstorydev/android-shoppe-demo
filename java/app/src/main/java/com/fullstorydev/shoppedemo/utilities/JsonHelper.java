5264abc6ee1b9993672d52562ab8cdf4230b0a7c
Sabrina Li <sabrinali0312@gmail.com>
2020-04-09 14:29:19 -0400

    remove logging and shorten code

--- java/app/src/main/java/com/fullstorydev/shoppedemo/utilities/JsonHelper.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/utilities/JsonHelper.java
@@ -13,11 +13,9 @@ public class JsonHelper {
     public static List<Item> getProductListFromJsonString(String jsonStr) throws JsonParseException {
         try{
             Gson gson = new Gson();
+            // for now item and product shares the same model, will separate them in the future refactor
             TypeToken<List<Item>> list = new TypeToken<List<Item>>() {};
-            // for now item and product shares the same model, may separate them in the future refactor
-            Log.d("here",jsonStr);
-            List<Item> availableProducts = gson.fromJson(jsonStr, list.getType());
-            return availableProducts;
+            return gson.fromJson(jsonStr, list.getType());
         }catch (JsonParseException e){
             throw e;
         }
