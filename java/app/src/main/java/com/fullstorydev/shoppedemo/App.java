ef831bb61d1c685e5e41b17bd422cc28c153910b
Sabrina Li <sabrinali0312@gmail.com>
2020-04-09 12:32:07 -0400

    add segment dependencies

--- java/app/src/main/java/com/fullstorydev/shoppedemo/App.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/App.java
@@ -1,10 +1,24 @@
 package com.fullstorydev.shoppedemo;
 
 import androidx.multidex.MultiDexApplication;
+
+import com.segment.analytics.Analytics;
+
 public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        //custom code
+
+        // Segment integration
+        // Create an analytics client with the given context and Segment write key.
+        Analytics analytics = new Analytics.Builder(getApplicationContext(), BuildConfig.)
+                // Enable this to record certain application events automatically!
+                .trackApplicationLifecycleEvents()
+                // Enable this to record screen views automatically!
+                .recordScreenViews()
+                .build();
+
+// Set the initialized instance as a globally accessible instance.
+        Analytics.setSingletonInstance(analytics);
     }
 }
---
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/utilities/APIKey.java
@@ -0,0 +1,9 @@
+package com.fullstorydev.shoppedemo.utilities;
+
+public class APIKey {
+    // !! for dev purpose only !!
+    // Having your key here prevents it from being committed to git.
+    // However this is not secure and if someone were to decompile your app they can get to your API key
+    // Please follow the best practices to properly store and obfuscate your API Key
+    public static String SEGMENT_WRITE_KEY = "TOP SECRET";
+}
