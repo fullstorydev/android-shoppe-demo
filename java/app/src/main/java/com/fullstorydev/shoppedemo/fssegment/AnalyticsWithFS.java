36624fb102b69bb1ea0ad2b155adb81f2fc22577
Sabrina Li <sabrinali0312@gmail.com>
2020-04-10 18:55:34 -0400

    add track and reset in fs-segment class

--- java/app/src/main/java/com/fullstorydev/shoppedemo/fssegment/AnalyticsWithFS.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/fssegment/AnalyticsWithFS.java
@@ -4,6 +4,11 @@ import android.content.Context;
 
 import com.fullstory.FS;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Options;
+import com.segment.analytics.Properties;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnalyticsWithFS {
     private Analytics analytics;
@@ -32,4 +37,31 @@ public class AnalyticsWithFS {
         this.analytics.identify(id);
         FS.identify(id);
     }
+
+    public void reset(){
+        this.analytics.reset();
+        FS.anonymize();
+    }
+
+    public void track(String name){
+        this.analytics.track(name);
+        name = "Segment-" + name;
+        FS.event(name,new HashMap<>());
+    }
+
+    public void track(String name, Properties properties){
+        this.analytics.track(name, properties);
+        name = "Segment-" + name;
+        FS.event(name,properties);
+    }
+
+    public void track(String name, Properties properties, Options options){
+        this.analytics.track(name,properties,options);
+        name = "Segment-" + name;
+        HashMap eventFS = new HashMap();
+        eventFS.put("properties",properties);
+        eventFS.put("options",options);
+        FS.event(name,eventFS);
+    }
+
 }
