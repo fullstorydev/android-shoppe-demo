fb2d335a24b25c8d17e92c788f28d878ca02a4cc
sabrina-li <sabrinali0312@gmail.com>
2020-04-10 16:56:52 -0400

    remove logging and shorten code (#49)
    * remove logging and shorten code
    
    * remove log package

--- java/app/src/main/java/com/fullstorydev/shoppedemo/utilities/JsonHelper.java
+++ java/app/src/main/java/com/fullstorydev/shoppedemo/utilities/JsonHelper.java
@@ -1,7 +1,5 @@
 package com.fullstorydev.shoppedemo.utilities;
 
-import android.util.Log;
-
 import com.fullstorydev.shoppedemo.data.Item;
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
@@ -13,11 +11,9 @@ public class JsonHelper {
     public static List<Item> getProductListFromJsonString(String jsonStr) throws JsonParseException {
         try{
             Gson gson = new Gson();
+            // for now item and product shares the same model, will separate them in the future refactor
             TypeToken<List<Item>> list = new TypeToken<List<Item>>() {};
-            // for now item and product shares the same model, may separate them in the future refactor
-            Log.d("here",jsonStr);
-            List<Item> availableProducts = gson.fromJson(jsonStr, list.getType());
-            return availableProducts;
+            return gson.fromJson(jsonStr, list.getType());
         }catch (JsonParseException e){
             throw e;
         }
