8138d664a4096dd33b599933e226bcc07478334d
MattW <mattwear@fullstory.com>
2021-10-11 16:57:52 -0500

    Added FS sdk to Kotlin project and ensured it could compile, updated libs

---
+++ kotlin/app/src/main/java/com/fullstorydev/shoppedemo/App.kt
@@ -0,0 +1,10 @@
+package com.fullstorydev.shoppedemo
+
+import androidx.multidex.MultiDexApplication
+
+class App : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        //custom code
+    }
+}
